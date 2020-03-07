78fb6ca15f7c209e96c83f6b4c752afd329f09ad
Cornean Dragos <dragoscornean@yahoo.com>
2020-03-07 18:32:25 +0200

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['react-hooks'],
+  globals: {
+    __DEV__: false,
+    fetch: false,
+    document: false,
+    window: false,
+    sessionStorage: false,
+    localStorage: false,
+    localStorage: false
+  },
+  ignorePatterns: [
+    'public/',
+    'src/libraries/',
+    '*.svg',
+    '*.css',
+    '*.ai',
+    'serviceWorker'
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-closing-bracket-location': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/no-array-index-key': 0,
+    'react/sort-comp': 0,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'prefer-const': 0,
+    'no-underscore-dangle': 0,
+    'no-plusplus': 0,
+    'import/prefer-default-export': 0,
+    'class-methods-use-this': 0,
+    'react/jsx-filename-extension': 0
+  }
+};
